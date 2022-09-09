e3bca14d8a847c61e266e65eafc896db26522274
Roman Chugunov <roman.chugunov@cteleport.com>
2022-09-09 19:13:58 +0200

    Simplified code, one url checked

--- parser.py
+++ parser.py
@@ -12,6 +12,9 @@ from random import choice
 
 bot_token = ""
 chat_id = ""
+url = "https://holland2stay.com/residences.html?available_to_book=179"
+
+filters = ["maastricht", "rive tower 1"]
 
 
 def get_current_hour():
@@ -31,17 +34,16 @@ def write_last_alive_date(date: str):
 def send_telegram_message(message):
     requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}')
 
-def get_list_of_residences_texts(driver):
-    url = "https://holland2stay.com/residences.html?available_to_book=179"
+def get_list_of_residences_texts(driver, filters):
     driver.get(url)
     items_list = driver.find_element(By.CLASS_NAME, "regi-list")
     items = items_list.find_elements(By.CLASS_NAME, "regi-item") 
-    items = [item.text for item in items if "book directly" in item.text.lower()]
+    items = [item.text for item in items if "book directly" in item.text.lower() and \
+             all(flt not in item.text.lower() for flt in filters)]
     return items
 
 
 try:
-    url = "https://holland2stay.com/residences.html"
 
     user_agent = "Mozilla/5.0 (X11; Linux i686; rv:104.0) Gecko/20100101 Firefox/104.0"
     options = Options()
@@ -51,18 +53,9 @@ try:
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
     driver.set_page_load_timeout(30)
     print("Firefox started")
-    driver.get(url)
-    print("Page loaded")
-    attribute_value = "available_to_book"
-    text = "Available to book"
-    print("Searching for element")
-    filters_panel = driver.find_element(by=By.CSS_SELECTOR, value=f"div[attribute='{attribute_value}']")
-    total_links = len(filters_panel.find_elements(By.TAG_NAME, "a"))
-    print(f"Found {total_links} links")
-    if text in filters_panel.text or total_links > 2:
-        residences_texts = get_list_of_residences_texts(driver)
-        send_telegram_message(f"""There are {len(residences_texts)} apartment(s) available to book directly."""
-                              f"""\n{url}\nRandom apartment:\n{choice(residences_texts)}""")
+    residences_texts = get_list_of_residences_texts(driver, filters)
+    if residences_texts:
+        send_telegram_message(f"""There are {len(residences_texts)} apartment(s) available to book directly (except {', '.join(filters)}).\n{url}\nRandom apartment:\n{choice(residences_texts)}""")
     today = str(datetime.datetime.now().date())
     last_alive_date = read_last_alive_date()
     if (get_current_hour() >= 9) and (((last_alive_date is not None) and (last_alive_date < today)) or (last_alive_date is None)):
@@ -79,3 +72,4 @@ finally:
     # remove logs
     if os.path.exists("geckodriver.log"):
         os.remove("geckodriver.log")
+
